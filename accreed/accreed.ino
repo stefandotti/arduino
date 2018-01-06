64faf970c70b785869822dd84d3c458c3efb12fb
Stefan Dotti <stefan@dotti.at>
2018-01-06 09:02:30 +0100

    acc with reed speed meter

--- accreed/accreed.ino
+++ accreed/accreed.ino
@@ -18,30 +18,14 @@
 #define DELAY   20
 #define BASE    0
 #define DELTA   1
-
-#define XOFFSET 0
-#define YOFFSET -500
-#define ZOFFSET 1800
-
 #define AVG     1
 
-int level = 300;
-
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-#include<Wire.h>
-const int MPU_addr = 0x68; // I2C address of the MPU-6050
-int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
-  Wire.begin();
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
-  Wire.endTransmission(true);
   pinMode(pinSwitch, INPUT); // reed
   //Serial.begin(9600);
-  readmpu();
-  level = AcY + 100;
 }
 
 int old_x = 0, old_y = 0, old_z = 0;
@@ -140,24 +124,5 @@ void loop() {
   if (l >= NUMPIXELS) l = 0;
 
   delay(DELAY);
-}
-
-void readmpu() {
-  AcX = 0; AcY = 0; AcZ = 0; Tmp = 0; GyX = 0; GyY = 0; GyZ = 0;
-  for (int n = 0; n < AVG; n++) {
-    Wire.beginTransmission(MPU_addr);
-    Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
-    AcX += Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-    AcY += Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-    AcZ += Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-    Tmp += Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-    GyX += Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-    GyY += Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-    GyZ += Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-    delay(DELAY / AVG);
-  }
-  AcX /= AVG; AcY /= AVG; AcZ /= AVG; Tmp /= AVG; GyX /= AVG; GyY /= AVG; GyZ /= AVG;
 }
 
